7e46e0c12d72bac185e2b35e0232db3d6d71137f
wangilamain <wangilayng@gmail.com>
2023-01-04 09:57:18 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fuzzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fuzzbuzz.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""Print the numbers from 1 to 100 separated by a space.
+  For multiples of three, print Fizz instead of the number
+  For multiples of five, print Buzz instead of the number.
+  For multiples of three and five, print FizzBuzz instead of the number.
+  """
+
+
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-             print("FizzBuzz", end=" ")
-         elif num % 3 == 0:
-             print("Fizz", end=" ")
-              elif num % 5 == 0:
-                  print("Buzz", end=" ")
-              else:
-                  print(num, end=" ")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
